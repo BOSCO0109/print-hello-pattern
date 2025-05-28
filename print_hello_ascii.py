a993a28e30398c9d9f5c21c0bd1aa6d134371c67
BOSCO0109 <boscodon321@gmail.com>
2025-05-28 16:05:49 +0530

    Update print_hello_ascii.py

--- print_hello_ascii.py
+++ print_hello_ascii.py
@@ -1,5 +1,5 @@
 # print-hello-pattern
-This Python script prints the word "HELLO" using ASCII characters and nested loops. It demonstrates the use of conditional logic, for loops, and pattern printing in a creative way. Ideal for beginners learning Python control structures and character-based output.
+#This Python script prints the word "HELLO" using ASCII characters and nested loops. It demonstrates the use of conditional logic, for loops, and pattern printing in a creative way. Ideal for beginners learning Python control structures and character-based output.
 
 
 # This code is show us the usage of union , and ,not ,or and else function  
